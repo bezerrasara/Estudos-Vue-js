a5c2ae75addcef5639650769ce682ab77ca48119
Sara Moreira <morirasara340@gmail.com>
2022-08-31 13:32:53 -0300

    Otimizando o vuex

---
+++ src/store/mutations.js
@@ -0,0 +1,23 @@
+import db from '../services/localbase.js';
+
+export default {
+buscaTarefas(state){
+    db.collection('tarefas').get().then(tarefasDB => {
+        state.tarefas = tarefasDB
+    })
+    
+
+    },
+    adicionaTarefa(state, titulo){
+        db.collection('tarefas').add({
+            id: new Date().getTime(),
+            titulo,
+            concluido: false
+     })
+    },
+
+    //unica que ta funcionando pra deletar
+    removeTarefa(state, id){
+         state.tarefas = state.tarefas.filter(tarefa => tarefa.id!==id)
+         },
+}
